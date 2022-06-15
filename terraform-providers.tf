1f92db87f0ade2395455b366fe43359f5bb877df
Andy Li <andy@onthewings.net>
2022-06-15 10:53:17 +0800

    install oci-cli, remove ansible

--- terraform-providers.tf
+++ terraform-providers.tf
@@ -1,10 +1,16 @@
 terraform {
   required_providers {
     cloudflare = {
-      source = "cloudflare/cloudflare"
-      version = "~> 3.4"
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.16"
+    }
+    gitlab = {
+      source  = "gitlabhq/gitlab"
+      version = "~> 3.15"
+    }
+    oci = {
+      source  = "oracle/oci"
+      version = "~> 4.79"
     }
   }
 }
-
-provider "cloudflare" {}
